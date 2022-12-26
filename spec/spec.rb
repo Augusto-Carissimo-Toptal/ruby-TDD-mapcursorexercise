a735125e71206463ef3a9fff4e68c2ea11b086b8
Augusto-Carissimo_Toptal <augusto.carissimo@toptal.com>
2022-12-26 17:14:34 -0300

    Add assert_Map

--- spec/spec.rb
+++ spec/spec.rb
@@ -2,9 +2,13 @@ require_relative "./../class/map"
 require_relative "./../class/cursor"
 
 class Test
+
+  def assert_Map
+    raise '' unless Map.new(5,5,nil)
+  end
   
   def assert_move_Map
-    raise '' unless Map.new(5,5, Cursor.new(3,3))
+    raise '' unless Map.new(5,5, Cursor.new(3,3)).move('left') == Map.new(5,5, Cursor.new(2,3))
   end
 end
 
