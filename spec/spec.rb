c5bb20a4caef776abf10a0f720f554ceb63f1501
Augusto-Carissimo_Toptal <augusto.carissimo@toptal.com>
2022-12-26 17:44:19 -0300

    Add assert_Cursor in Test

--- spec/spec.rb
+++ spec/spec.rb
@@ -2,15 +2,23 @@ require_relative "./../class/map"
 require_relative "./../class/cursor"
 
 class Test
-
+  
   def assert_Map
-    raise '' unless Map.new(5,5,nil)
+    map = Map.new(5,5,Cursor.new(3,3))
+    raise "#{map.inspect}" unless map
+  end
+
+  def assert_Cursor
+    cursor = Cursor.new(3,3)
+    raise "#{cursor.inspect}" unless cursor
   end
   
   def assert_move_Map
-    raise '' unless Map.new(5,5, Cursor.new(3,3)).move('left') == Map.new(5,5, Cursor.new(2,3))
+    map = Map.new(5,5, Cursor.new(3,3))
+    raise "#{map.inspect}" unless map.move('left').cursor.x == Map.new(5,5, Cursor.new(2,3)).cursor.x
   end
 end
 
 test = Test.new
+test.assert_Map
 test.assert_move_Map
