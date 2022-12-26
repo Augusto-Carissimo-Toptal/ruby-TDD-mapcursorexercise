0c8a7222402f087bb24820925894863121f34533
Augusto-Carissimo_Toptal <augusto.carissimo@toptal.com>
2022-12-26 17:52:16 -0300

    Refactor Test

--- spec/spec.rb
+++ spec/spec.rb
@@ -3,22 +3,27 @@ require_relative "./../class/cursor"
 
 class Test
   
-  def assert_Map
-    map = Map.new(5,5,Cursor.new(3,3))
+  def assert_Map(map)
     raise "#{map.inspect}" unless map
   end
 
-  def assert_Cursor
-    cursor = Cursor.new(3,3)
+  def assert_Cursor(cursor)
     raise "#{cursor.inspect}" unless cursor
   end
   
-  def assert_move_Map
-    map = Map.new(5,5, Cursor.new(3,3))
-    raise "#{map.inspect}" unless map.move('left').cursor.x == Map.new(5,5, Cursor.new(2,3)).cursor.x
+  def assert_move_Map(map, direction, new_map)
+    if direction == 'left' && direction == 'right'
+      raise "#{map.inspect}" unless map.move(direction).cursor.x == new_map.cursor.x
+    elsif direction == 'down' && direction == 'up'
+      raise "#{map.inspect}" unless map.move(direction).cursor.y == new_map.cursor.y
+    end
   end
 end
 
 test = Test.new
-test.assert_Map
-test.assert_move_Map
+test.assert_Map(Map.new(5,5,Cursor.new(3,3)))
+test.assert_Cursor(Cursor.new(3,3))
+test.assert_move_Map(Map.new(5,5, Cursor.new(3,3)), 'left', Map.new(5,5, Cursor.new(2,3)))
+test.assert_move_Map(Map.new(5,5, Cursor.new(3,3)), 'right', Map.new(5,5, Cursor.new(4,3)))
+test.assert_move_Map(Map.new(5,5, Cursor.new(3,3)), 'down', Map.new(5,5, Cursor.new(3,2)))
+test.assert_move_Map(Map.new(5,5, Cursor.new(3,3)), 'up', Map.new(5,5, Cursor.new(3,4)))
