8551d6864e3cf511e83e937703c358d8becefd3e
Augusto-Carissimo_Toptal <augusto.carissimo@toptal.com>
2022-12-26 17:40:45 -0300

    Refactor move method

--- class/map.rb
+++ class/map.rb
@@ -1,21 +1,24 @@
+require_relative "./cursor"
+
 class Map
   attr_reader :height, :width, :cursor
 
   def initialize(height, width, cursor)
     @height = height
     @width = width
+    @cursor = cursor
   end
 
   def move(direction)
     case direction
     when "left"
-      return Map.new(5,5, Cursor.new(x-1,y))
+      return Map.new(5,5, Cursor.new(@cursor.x - 1, @cursor.y))
     when "right"
-      return Map.new(5,5, Cursor.new(x+1,y))
-    when "up"
-      return Map.new(5,5, Cursor.new(x,y+1))
+      return Map.new(5,5, Cursor.new(@cursor.x + 1, @cursor.y))
     when "down"
-      return Map.new(5,5, Cursor.new(x,y-1))
+      return Map.new(5,5, Cursor.new(@cursor.x, @cursor.y - 1))
+    when "up"
+      return Map.new(5,5, Cursor.new(@cursor.x, @cursor.y + 1))
     end
   end
 
